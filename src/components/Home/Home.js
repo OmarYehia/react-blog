64a37a41841732db59f38a0485acddb3010cc4aa
Omar Yehia <oyehia94@gmail.com>
2021-04-17 23:31:20 +0200

    Initial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import useFetch from "../../hooks/useFetch";
+import BlogList from "../BlogList/BlogList";
+import "./Home.css";
+
+const Home = () => {
+  const { data: blogs, isPending, error } = useFetch(
+    "http://localhost:8000/blogs"
+  );
+
+  return (
+    <div className="home">
+      {error && <div> {error} </div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
+
+export default Home;
